01b0280d4bed81deef4cb6ecc2b7b992e9947b3c
Tugdual Grall <tgrall@github.com>
2021-10-17 10:36:54 +0200

    new

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const core = require('@actions/core');
 const action = require('./action');
 
-(async () => {
-    try {
-        await action();
-    } catch (error) {
-        core.setFailed(error.message);
-    }
-})();
 
 
+
+
+
+
+async function run() { 
+  const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+  const { context = {} } = github;
+  const { pull_request } = context.payload;
+
+  if ( !pull_request ) {
+    throw new Error('Could not find pull request!')
+  };
+
+  console.log(`Found pull request: ${pull_request.number}`);
+
+  const octokit = github.getOctokit(GITHUB_TOKEN)
+
+  await octokit.issues.createComment({
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: "Tug's comment"
+  });
+
+
+
+
+  // const result = await octokit.pulls.update(params);
+  core.setOutput("result", "DONE");
+}
+
+run().catch(e => core.setFailed(e.message));
