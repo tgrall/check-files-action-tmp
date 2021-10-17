353e9386118f305a7c2a6992e5c9bba2375919af
Tugdual Grall <tgrall@github.com>
2021-10-17 10:52:08 +0200

    n

--- action.js
+++ action.js
@@ -3,30 +3,73 @@ const github = require('@actions/github');
 
 
 const action = async () => {    
-    
-    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-    const { context = {} } = github;
-    const { pull_request } = context.payload;
-  
-    if ( !pull_request ) {
-      throw new Error('Could not find pull request!')
+    const gitHubToken = core.getInput('github_token');
+    const owner = core.getInput('owner');
+    const repo = core.getInput('repo');
+    const pull_number = core.getInput('pull_number');
+    const branch = core.getInput('branch');
+    const title = core.getInput('title');
+    const body = core.getInput('body');
+    const labels = core.getInput('labels');
+    const assignees = core.getInput('assignees');
+    const milestone = core.getInput('milestone');
+    const state = core.getInput('state');
+    const draft = core.getInput('draft');
+    const maintainer_can_modify = core.getInput('maintainer_can_modify');
+    const head = core.getInput('head');
+    const base = core.getInput('base');
+
+
+    const commit = core.getInput('commit');
+    const pullRequest = github.context.payload.pull_request;
+    const link = (pullRequest && pullRequest.html_url) || github.context.ref;    
+    const name = core.getInput('check_name');
+    const status = 'completed';
+    const head_sha = commit || (pullRequest && pullRequest.head.sha) || github.context.sha;
+
+    const conclusion =  "success";
+
+    core.info(
+        `Posting status '${status}' with conclusion '${conclusion}' to ${link} (sha: ${head_sha})`
+    );
+    const octokit = github.getOctokit(gitHubToken);
+
+    const createCheckRequest = {
+        ...github.context.repo,
+        "My Check",
+        head_sha,
+        status,
+        conclusion,
+        output: {
+            title,
+            summary: "Tug's summary",
+            annotations: "Tug Annotations"
+        }    
     };
-  
-    console.log(`Found pull request: ${pull_request.number}`);
-  
-    const octokit = github.getOctokit(GITHUB_TOKEN)
-  
-    await octokit.issues.createComment({
-      ...context.repo,
-      issue_number: pull_request.number,
-      body: "Tug's comment"
-    });
+
+    core.info(JSON.stringify(createCheckRequest, null, 2));
+    await octokit.rest.checks.create(createCheckRequest);
 
 
 
+    const params = {
+        owner: owner,
+        repo: repo,
+        pull_number: pull_number,
+        base: base,
+        head: head,
+        title: title,
+        body: body,
+        state: state,
+        draft: draft,
+        maintainer_can_modify: maintainer_can_modify,
+        labels: labels,
+        assignees: assignees,
+        milestone: milestone
+    };
 
     // const result = await octokit.pulls.update(params);
-    core.setOutput("result", "DONE");
+    core.setOutput("result", JSON.stringify(params));
 
 }
 
--- index.js
+++ index.js
@@ -1,37 +1,10 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const action = require('./action');
 
-
-
-
-
-
-
-async function run() { 
-  const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-  const { context = {} } = github;
-  const { pull_request } = context.payload;
-
-  if ( !pull_request ) {
-    throw new Error('Could not find pull request!')
-  };
-
-  console.log(`Found pull request --->>>> ${pull_request.number}`);
-
-  const octokit = github.getOctokit(GITHUB_TOKEN)
-
-
-  await octokit.rest.issues.createComment({
-    ...context.repo,
-    issue_number: pull_request.number,
-    body: "Tug's comment"
-  });
-
-
-
-
-  // const result = await octokit.pulls.update(params);
-  core.setOutput("result", "DONE");
-}
-
-run().catch(e => core.setFailed(e.message));
+(async () => {
+    try {
+        await action();
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+})();
